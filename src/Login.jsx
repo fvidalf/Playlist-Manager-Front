b345e20be6a0cb43dbfeccd7031f663cbd02bbc1
NavaUDP <diego.navarrete2@mail.udp.cl>
2024-07-23 19:14:03 -0400

    modificaciones esteticas

--- src/Login.jsx
+++ src/Login.jsx
@@ -3,8 +3,8 @@ import "./styles.css"
 export default function App() {
   return (
     <>
-    <h1>Bienvenido a Playlist Manager, inicie sesión para continuar</h1>
-    <button>Log In con Spotify</button>
+    <h1> Bienvenido a Playlist Manager, inicie sesión para continuar </h1>
+    <button class="spoti-button">Log In con Spotify</button>
   </>
   )
 }
