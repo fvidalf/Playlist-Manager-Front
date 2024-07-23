bda808e52fc3551399e7b5df66343aa4d6e89215
NavaUDP <diego.navarrete2@mail.udp.cl>
2024-07-23 15:58:14 -0400

    estilizacion login

---
+++ src/Login.jsx
@@ -0,0 +1,10 @@
+import "./styles.css"
+
+export default function App() {
+  return (
+    <>
+    <h1>Bienvenido a Playlist Manager, inicie sesión para continuar</h1>
+    <button>Log In con Spotify</button>
+  </>
+  )
+}
