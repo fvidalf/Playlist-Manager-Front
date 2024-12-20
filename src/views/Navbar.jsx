ddf74f3d1eeb3af2884f6209717635529ec8c329
fvidalf <fvidalf@uc.cl>
2024-12-19 23:05:24 -0300

    feat(navbar-styling): Add navbar with more complete styling. Refactor general page styling to allow easier navbar integration

--- src/views/Navbar.jsx
+++ src/views/Navbar.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useAuth } from '../context/AuthProvider';
 
 
 const Navbar = () => {
+  const { isAuthenticated } = useAuth();
+
   return (
     <nav className="">
-      <a className="" href="/">Playlist Manager</a>
-      <Link to="/playlists">Playlists</Link>
-      <Link to="/login">Login</Link>
+      <a className="navbar-item" href="/">Playlist Manager</a>
+      {isAuthenticated && <Link className="navbar-item" to="/playlists">Your Playlists</Link>}
+      {!isAuthenticated && <Link className="navbar-item" to="/login">Login</Link>}
     </nav> 
   )
 
